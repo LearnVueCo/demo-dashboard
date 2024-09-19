beb5d524544c8b826b5df5f835e46101df1a1888
Matt Maribojoc <matthewmaribojoc@gmail.com>
2024-09-18 21:03:37 -0400

    remove auth code

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,3 @@
-<script setup lang="ts">
-const { loggedIn, user, session, fetch, clear } = useUserSession()
-</script>
 <template>
   <div class="bg-gray-950 text-gray-200 min-h-screen">
     <ClientOnly>
